ae3b00c36db4c56c1e7e077ee8ad02d203690388
Scill <realscill@gmail.com>
2025-02-22 23:41:46 -0500

    Remove unused background logic

--- src/page/home/hero/Background.tsx
+++ src/page/home/hero/Background.tsx
@@ -1,22 +1,16 @@
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
 import GLOBE from "vanta/src/vanta.globe";
-import { AnimationStage } from "../util/animation";
 
-type Props = {
-    stage: AnimationStage;
-};
-
-export function Background({ stage }: Props) {
+export function Background() {
     const vantaRef = useRef<HTMLDivElement>(null);
-    const vantaEffectRef = useRef<any>(null);
 
     useEffect(() => {
         if (!vantaRef.current) {
             return;
         }
 
-        vantaEffectRef.current = GLOBE({
+        const effect = GLOBE({
             THREE,
             el: vantaRef.current,
             backgroundColor: "white",
@@ -27,21 +21,12 @@ export function Background({ stage }: Props) {
             touchControls: false,
             gyroControls: false,
         });
-        vantaEffectRef.current.cont2.position.x -= 13;
-        vantaEffectRef.current.cont2.position.y -= 10;
-        vantaEffectRef.current.cont2.position.z -= 5;
+        effect.cont2.position.x -= 13;
+        effect.cont2.position.y -= 10;
+        effect.cont2.position.z -= 5;
 
-        return () => vantaEffectRef.current.destroy();
+        return () => effect.destroy();
     }, []);
-
-    useEffect(() => {
-        if (stage === AnimationStage.TextExplode) {
-            vantaEffectRef.current.setOptions({
-                chaos: 5,
-            });
-        }
-        vantaEffectRef.current.resize();
-    }, [stage]);
 
     return <div ref={vantaRef} className="left absolute -z-10 h-full w-full opacity-40" />;
 }
