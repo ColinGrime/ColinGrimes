8600a7c7c2d8686739718fd52bbe73801df54ef9
Scill <realscill@gmail.com>
2025-02-23 14:15:09 -0500

    Add front page content

--- src/page/home/hero/Hero.tsx
+++ src/page/home/hero/Hero.tsx
@@ -13,24 +13,41 @@ export default function Hero({ stage, setStage }: Props) {
         <div className="relative flex h-full flex-grow items-center justify-center">
             {stage !== AnimationStage.Completed && <IntroAnimation stage={stage} setStage={setStage} />}
             <div className="h-full w-full">
-                <div className={`absolute h-full w-full transition-opacity duration-2000 xl:block ${!isPageReady(stage) && "opacity-0"}`}>
+                <div className={`absolute hidden h-full w-full transition-opacity duration-2000 xl:block ${!isPageReady(stage) && "opacity-0"}`}>
                     <Background stage={stage} />
                 </div>
                 {isPageReady(stage) && (
                     <div className="absolute top-30 left-20 flex w-125 flex-col gap-5 self-start">
-                        <h1 className="text-3xl font-bold">Hello!</h1>
-                        <div>
-                            <p>I'm Colin.</p>
-                            <p>I'm a software engineer.</p>
+                        <h1 className="text-3xl font-bold">Welcome!</h1>
+                        <p>
+                            I'm Colin.
+                            <br />
+                            I'm a software developer.
+                        </p>
+                        <p>I have experience with a number of languages and libraries:</p>
+                        <div className="grid h-50 w-110 grid-cols-6 grid-rows-3 gap-x-5">
+                            <img src="programming/java.svg" />
+                            <img src="programming/python.svg" />
+                            <img src="programming/typescript.svg" />
+                            <img src="programming/javascript.svg" />
+                            <img src="programming/selenium.svg" />
+                            <img src="programming/node.svg" />
+                            <img src="programming/react.svg" />
+                            <img src="programming/tailwindcss.svg" />
+                            <img src="programming/material-ui.svg" />
+                            <img src="programming/jest.svg" />
+                            <img src="programming/openlayers.svg" />
+                            <img src="programming/electron.svg" />
+                            <img src="programming/gradle.svg" />
+                            <img src="programming/maven.svg" />
+                            <img src="programming/sql.svg" />
+                            <img src="programming/postgresql.svg" />
+                            <img src="programming/docker.svg" />
+                            <img src="programming/git.svg" />
                         </div>
                         <p>
-                            Mollit magna enim minim. In ad sunt quis cupidatat irure consectetur ut adipisicing ut proident aliqua commodo dolor laborum nulla.
-                            Quis velit incididunt irure nulla excepteur nulla eiusmod nostrud nostrud. Sunt amet quis consectetur elit mollit ullamco nostrud ea
-                            laborum cillum sit aute in laborum. Qui eiusmod nisi tempor. Anim nulla veniam occaecat dolore aliqua qui anim officia eiusmod
-                            officia ullamco. Irure occaecat dolore dolore sit sunt tempor non ullamco. Reprehenderit eiusmod ut cupidatat fugiat est esse ipsum
-                            nulla nisi cillum. Mollit non aliqua aute et aliqua nulla laboris incididunt incididunt proident commodo deserunt. Occaecat sit
-                            aliqua ex id mollit deserunt. Nostrud ea officia veniam eiusmod duis nostrud mollit consectetur ad. Cupidatat incididunt consequat
-                            Lorem. Quis irure anim sunt. Pariatur tempor in pariatur ipsum.
+                            I'm passionate about clean code, scalable architecture, and efficient design patterns, and I enjoy exploring new technologies to
+                            expand my skill set.
                         </p>
                     </div>
                 )}
